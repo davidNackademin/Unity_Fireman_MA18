97537d9f8368435b568606a95130852797698811
David Svensson <davidsvson@gmail.com>
2019-08-23 10:37:04 +0200

    fireman positions

---
+++ Assets/_Game/Scripts/ButtonInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInput : MonoBehaviour
+{
+    public bool left;
+
+    private void OnMouseDown()
+    {
+        if (left)
+        {
+            Debug.Log("touch left!");
+        } else
+        {
+            Debug.Log("Touch Right");
+        }
+
+        
+    }
+
+
+}
---
+++ Assets/_Game/Scripts/FiremanController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiremanController : MonoBehaviour
+{
+
+    public List<Transform> positions = new List<Transform>();
+
+    private void Start()
+    {
+        transform.position = positions[1].position;
+    }
+
+
+}
